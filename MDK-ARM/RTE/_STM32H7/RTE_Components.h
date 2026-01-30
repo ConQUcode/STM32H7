e1c811d24ff013386db83fbfd614b0617a8926c1
章铭宇 <15234223+zmyaaaz@user.noreply.gitee.com>
2026-01-31 02:05:36 +0800

    第一次提交，完成了bsp_fdcan的移植和大疆电机的移植，还有一些报错

---
+++ MDK-ARM/RTE/_STM32H7/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32H7' 
+ * Target:  'STM32H7' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
